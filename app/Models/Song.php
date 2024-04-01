ba11135bce2030dda91be7f984bde83e45a83155
Long Nguyễn Thành <thanhlong210494@gmail.com>
2024-04-01 12:48:07 +0700

    first commit

---
+++ app/Models/Song.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Song extends Model
+{
+    use HasFactory;
+    public $timestamps = true;
+    protected $hidden = ['pivot'];
+    protected $fillable = [
+        'name',
+        'file',
+        'thumbnail'
+    ];
+    public function singers()
+    {
+        return $this->belongsToMany(Singer::class);
+    }
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}

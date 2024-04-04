a34017a79c9255f53a6b56b7ca5bcc5b1eb37c7b
Long Nguyễn Thành <thanhlong210494@gmail.com>
2024-04-04 19:13:48 +0700

    edit song name

--- resources/views/songs/edit.blade.php
+++ resources/views/songs/edit.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="mb-3">
             <label for="photo" class="form-label">Thumbnail</label>
-            <input type="thumbnail" class="form-control" id="photo" name="file">
+            <input type="file" class="form-control" id="photo" name="thumbnail">
         </div>
 
         <!-- <label for="file">file</label>
